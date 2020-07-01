289db93f0c5e7bdcc71585685709b91505c63d95
KostiantynVorobiov <pyteec.ua@i.ua>
2020-07-01 21:32:39 +0300

    add method success

--- src/main/java/APP.java
+++ src/main/java/APP.java
@@ -2,5 +2,9 @@ public class APP {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("Hello mates!");
+        System.out.println(success());
+    }
+    private static String success() {
+        return "I did it!!!!";
     }
 }
